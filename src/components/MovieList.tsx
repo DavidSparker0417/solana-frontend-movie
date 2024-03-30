72f58974909a5b68a7d652659dd6d4c40337b2d7
DavidSparker <davidsparker0417@gmain.com>
2024-03-31 05:14:23 +0900

    - pagination, search function added

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -3,22 +3,52 @@ import { FC, useEffect, useState } from "react";
 import { Card } from "./Card";
 import { useConnection } from "@solana/wallet-adapter-react";
 import { MovieCoordinator } from "@/coordinators/MovieCoordinators";
+import { Button, Center, HStack, Input, Spacer } from "@chakra-ui/react";
 
 export const MovieList: FC = () => {
   const [movies, setMovies] = useState<Movie[]>([])
   const { connection } = useConnection()
+  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    MovieCoordinator.fetchPage(connection)
-    .then(movies => {
+    MovieCoordinator.fetchPage(
+      connection,
+      page,
+      10,
+      search,
+      search !== ''
+    ).then(movies => {
       setMovies(movies)
     });
-  }, [])
+  }, [page, search])
   return (
     <div>
+      <Center>
+        <Input
+          id = 'search'
+          color="gray.400"
+          onChange={event => setSearch(event.currentTarget.value)}
+          placeholder="Search"
+          w = "97%"
+          my={2}
+        />
+      </Center>
       {
         movies.map((movie, i) => <Card key={i} movie={movie} />)
       }
+      <Center>
+        <HStack w='full' mt={2} mb={8} ml={4} mr={4}>
+          {
+            page > 1 && <Button onClick={() => setPage(page - 1)}>Previous</Button>
+          }
+          <Spacer />
+          {
+            MovieCoordinator.accounts.length > page * 2 &&
+            <Button onClick={() => setPage(page + 1)}>Next</Button>
+          }
+        </HStack>
+      </Center>
     </div>
   )
 }
--- src/coordinators/MovieCoordinators.ts
+++ src/coordinators/MovieCoordinators.ts
@@ -1,29 +1,51 @@
 import { Movie } from "@/models/Movie"
 import * as web3 from "@solana/web3.js"
+import bs58 from "bs58"
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 export class MovieCoordinator {
   static accounts: web3.PublicKey[] = []
 
-  static async prefetchAccounts(connection: web3.Connection) {
+  static async prefetchAccounts(connection: web3.Connection, search: string = '') {
     const accounts = await connection.getProgramAccounts(
       new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID),
       {
         dataSlice: { offset: 2, length: 18 },
-        filters: []
+        filters: search === '' ? [] : [
+          {
+            memcmp: {
+              offset: 6,
+              bytes: bs58.encode(Buffer.from(search))
+            }
+          }
+        ]
       }
     )
+    const temp_accounts = [...accounts]
 
-    this.accounts = accounts.map(account => account.pubkey)
+    temp_accounts.sort((a:any, b:any) => {
+      const lengthA = a.account.data.readUInt32LE(0)
+      const lengthB = b.account.data.readUInt32LE(0)
+      const dataA = a.account.data.slice(4, 4 + lengthA)
+      const dataB = b.account.data.slice(4, 4 + lengthB)
+      return dataA.compare(dataB)
+    })
+    this.accounts = temp_accounts.map(account => account.pubkey)
   }
 
   static async fetchPage(
     connection: web3.Connection,
+    page: number,
+    perPage: number,
+    search: string = '',
     reload: boolean = false): Promise<Movie[]> {
     if (this.accounts.length == 0 || reload) {
-      await this.prefetchAccounts(connection)
+      await this.prefetchAccounts(connection, search)
     }
-    const paginatedPublicKeys = this.accounts.slice(0, 10)
+    const paginatedPublicKeys = this.accounts.slice(
+      (page - 1) * perPage,
+      page*perPage
+    )
     if (paginatedPublicKeys.length == 0){
       return []
     }
